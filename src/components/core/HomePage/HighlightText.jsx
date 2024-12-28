88adffa51f0fb4b8a78bf5e3e9be6e41f2abae2e
prakhar17072004 <pmankur2004@gmail.com>
2024-12-28 23:09:27 +0530

    one component is updated

--- src/components/core/HomePage/HighlightText.jsx
+++ src/components/core/HomePage/HighlightText.jsx
@@ -1,10 +1,10 @@
-// import React from 'react'
+import React from 'react'
 
-// const HighlightText = ({text})=>{
-//       return(
-//         <span className='font-bold bg-blue-500'>
-//         {text}
-//       </span>
-//       )
-// }
-//  export default HighlightText;
+const HighlightText = ({text})=>{
+      return(
+        <span className='font-bold bg-blue-500'>
+        {text}
+      </span>
+      )
+}
+ export default HighlightText;
